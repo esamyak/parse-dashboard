107475825d7b52da607da416e31108ad8c77f602
Sam Tholiya <tholiya.sam@outlook.in>
2017-03-06 02:38:53 +0530

    Complete User

--- Parse-Dashboard/app.js
+++ Parse-Dashboard/app.js
@@ -86,10 +86,12 @@ module.exports = function(config, allowInsecureHTTP) {
         req.connection.remoteAddress === '127.0.0.1' ||
         req.connection.remoteAddress === '::ffff:127.0.0.1' ||
         req.connection.remoteAddress === '::1';
+      /*
       if (!requestIsLocal && !req.secure && !allowInsecureHTTP) {
         //Disallow HTTP requests except on localhost, to prevent the master key from being transmitted in cleartext
         return res.send({ success: false, error: 'Parse Dashboard can only be remotely accessed via HTTPS' });
       }
+      */
 
       if (!requestIsLocal && !users) {
         //Accessing the dashboard over the internet can only be done with username and password
